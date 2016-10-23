3560fa7c7046e0ed01a5d3a240b91795ad6fa143
guowenfh <guowen921@live.com>
2016-10-23 17:48:17 +0800

    更改路由设置方式

--- app.js
+++ app.js
@@ -13,36 +13,37 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 // 生成一个express实例 app。
 var app = express();
-
+// 设置端口号
+app.set('port', process.env.PORT || 3000);
 
 // 设置 views 文件夹为存放视图文件的目录
 app.set('views', path.join(__dirname, 'views'));
-
 // 使用art-template 的设置
 template.config('base', '');
 template.config('extname', '.html');
 app.engine('.html', template.__express);
 app.set('view engine', 'html');
-//app.set('views', __dirname + '/views');
 
-// 设置视图模板引擎为 ejs。
-// app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
+// favicon.ico存放位置
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
 // 加载日志中间件。
 app.use(logger('dev'));
 // 加载解析json的中间件。
 app.use(bodyParser.json());
 // 加载解析urlencoded请求体的中间件。
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 // 加载解析cookie的中间件。
 app.use(cookieParser());
 // 设置public文件夹为存放静态文件的目录。
 app.use(express.static(path.join(__dirname, 'public')));
 // 路由控制器。
-app.use('/', routes);
-app.use('/users', users);
+// app.use('/', routes);
+// app.use('/users', users);
+
+routes(app);
 
 // 捕获404错误，并转发到错误处理器。
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
-var express = require('express');
-var router = express.Router();
-
-/*生成一个路由实例用来捕获访问主页的GET请求，
-  导出这个路由并在app.js中通过app.use('/', routes); 加载。
-  这样，当访问主页时，就会调用res.render('index', { title: 'Express' })
-  ;渲染views/index.ejs模版并显示到浏览器中。*/
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-});
-
-module.exports = router;
+module.exports = function(app) {
+    app.get('/', function(req, res) {
+        res.render('index', { title: '主页' });
+    });
+    app.get('/reg', function(req, res) {
+        res.render('reg', { title: '注册' });
+    });
+    app.post('/reg', function(req, res) {});
+    app.get('/login', function(req, res) {
+        res.render('login', { title: '登录' });
+    });
+    app.post('/login', function(req, res) {});
+    app.get('/post', function(req, res) {
+        res.render('post', { title: '发表' });
+    });
+    app.post('/post', function(req, res) {});
+    app.get('/logout', function(req, res) {});
+};
