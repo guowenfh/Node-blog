f48958ef35c7c24a5088cfa20e3920873e8aaf24
guowenfh <chenguowen@raycloud.com>
2016-11-11 11:34:17 +0800

    文件实现

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var template = require('art-template')
-    // 引入session管理
+// 引入session管理
 var session = require('express-session')
 var MongoStore = require('connect-mongo')(session)
 
@@ -16,12 +16,12 @@ var routes = require('./routes/index')
 var settings = require('./settings')
 
 var flash = require('connect-flash')
-    // 生成一个express实例 app。
+// 生成一个express实例 app。
 var app = express()
 app.set('port', process.env.PORT || 3000) // 设置端口号
-    // 设置 views 文件夹为存放视图文件的目录
+// 设置 views 文件夹为存放视图文件的目录
 app.set('views', path.join(__dirname, 'views'))
-    // 使用art-template 的设置
+// 使用art-template 的设置
 template.config('base', '')
 template.config('extname', '.html')
 app.engine('.html', template.__express)
@@ -29,26 +29,27 @@ app.set('view engine', 'html')
 
 // flash 功能功能添加
 app.use(flash())
-    // favicon.ico存放位置
+// favicon.ico存放位置
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
-    // 加载日志中间件。
+// 加载日志中间件。
 app.use(logger('dev'))
-    // 加载解析json的中间件。
+// 加载解析json的中间件。
 app.use(bodyParser.json())
-    // 加载解析urlencoded请求体的中间件。
-app.use(bodyParser.urlencoded({ extended: true }))
-    // 加载解析cookie的中间件。
+// 加载解析urlencoded请求体的中间件。
+app.use(bodyParser.urlencoded({extended: true}))
+// 加载解析cookie的中间件。
 app.use(cookieParser())
-    // 设置public文件夹为存放静态文件的目录。
+// 设置public文件夹为存放静态文件的目录。
 app.use(express.static(path.join(__dirname, 'public')))
-    // 路由控制器。
-    // app.use('/', routes);
-    // app.use('/users', users);
+// 路由控制器。
+
+// app.use('/', routes);
+// app.use('/users', users);
 
 app.use(session({
     secret: settings.cookieSecret,
     key: settings.db, // cookie name
-    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 }, // 30 days
+    cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}, // 30 days
     store: new MongoStore({
         url: 'mongodb://localhost/blog',
     }),
@@ -62,7 +63,7 @@ routes(app)
 
 
 // 捕获404错误，并转发到错误处理器。
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     var err = new Error('Not Found')
     err.status = 404
     next(err)
@@ -71,7 +72,7 @@ app.use(function(req, res, next) {
 // error handlers
 // 开发环境下的错误处理器，将错误信息渲染error模版并显示到浏览器中。
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
+    app.use(function (err, req, res, next) {
         res.status(err.status || 500)
         res.render('error', {
             message: err.message,
@@ -81,15 +82,15 @@ if (app.get('env') === 'development') {
 }
 
 // 生产环境下的错误处理器，不会将错误信息泄露给用户。
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     res.status(err.status || 500)
     res.render('error', {
         message: err.message,
         error: {},
     })
 })
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'))
 })
-    // 导出app实例供其他模块调用。
+// 导出app实例供其他模块调用。
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,24 @@
 var crypto = require('crypto')
 var User = require('../models/user.js')
 var Post = require('../models/post.js')
+var multer = require('multer')
+
 module.exports = function(app) {
+    // 文件上传功能
+    var storage = multer.diskStorage({
+        // destination 是上传的文件所在的目录
+        destination: function (req, file, cb) {
+            cb(null, './public/images')
+        },
+        // filename 函数用来修改上传后的文件名，这里设置为保持原来的文件名。
+        filename: function (req, file, cb) {
+            cb(null, file.originalname)
+        }
+    })
+    var upload = multer({
+        storage: storage
+    })
+
     app.get('/', function(req, res) {
         Post.get(null, function(err, posts) {
             if (err) {
@@ -124,7 +141,20 @@ module.exports = function(app) {
         req.flash('success', '登出成功!')
         res.redirect('/') //登出成功后跳转到主页
     })
-
+    app.get('/upload', checkLogin)
+    app.get('/upload', function (req, res) {
+        res.render('upload', {
+            title: '文件上传',
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        })
+    })
+    app.post('/upload', checkLogin)
+    app.post('/upload', upload.array('field1', 5), function (req, res) {
+        req.flash('success', '文件上传成功!')
+        res.redirect('/upload')
+    })
     function checkLogin(req, res, next) {
         if (!req.session.user) {
             req.flash('error', '未登录!')
